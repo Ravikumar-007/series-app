ff8da7a017757c6dcb88d94aa1bacb32e24727ed
Ravi <inakumar@cisco.com>
2020-08-18 22:46:31 +0530

    Changed the api from two parametrs into one parameter

--- review-app/src/services/series.service.ts
+++ review-app/src/services/series.service.ts
@@ -1,27 +1,34 @@
-import { AxiosResponse } from 'axios';
-
 import * as helper from '../helpers/api.helper';
 
-export const fetchSeriesDetailsById = async (tvId: string, seasonId: string): Promise<AxiosResponse> => {
-    const response: any = {};
+export const fetchSeriesDetailsById = async (tvSeriesId: string): Promise<any> => {
+    const episodeResults: any = [];
     try {
-        // Calling the seasonApi using the axios library.
-        const fetchData = await helper.fetchApiData(tvId, seasonId);
-        // Returning empty object if no data found
-        if (!fetchData || !fetchData.data || fetchData.data.episodes.length === 0) return {} as any;
-        // Collecting the Data length.
-        const recordCounts = fetchData.data.episodes.length;
+        // Using Axios library getting the tv-series data
+        const seriesData = await helper.fetchSeriesData(tvSeriesId);
+        if (!seriesData || !seriesData.data || seriesData.data.seasons.length === 0) return {} as any;
+        const promises = seriesData.data.seasons.map(async (item: any) => {
+            const seasonData = await helper.fetchSeasonData(tvSeriesId, item.season_number);
+            return seasonData
+        })
+        const promiseResult = await Promise.all(promises);
+        promiseResult.forEach((entry: any) => {
+            entry.episodes.map((item: any) => {
+                episodeResults.push(item);
+            })
+        });
+        // Collecting the episodes data length.
+        const recordCounts = episodeResults.length;
         // Calculating the all the no.of votes for all the records
-        const voteCounts = fetchData.data.episodes.reduce((total: number, item: { vote_count: number; }) => total += item.vote_count, 0);
+        const voteCounts = episodeResults.reduce((total: number, item: { vote_count: number; }) => total += item.vote_count, 0);
         // Getting the avearge for calculating the top rated reviews.
         const voteCountsAvg = Math.floor(voteCounts / recordCounts);
         // Filtering the Object with counts average
-        let filteredRecords = fetchData.data.episodes.filter((item: any) => item.vote_count > voteCountsAvg);
+        let filteredRecords = episodeResults.filter((item: any) => item.vote_count > voteCountsAvg);
         // Sorting(Decending Order ) the Records based on the vote_average
         filteredRecords.sort((a: any, b: any) => (a.vote_average > b.vote_average) ? -1 : 1);
         // Checking the sorted data for beyond the limit (limit = 20) and merging with the previous record
         if (filteredRecords.length < 20) {
-            const remainingRecords = await fetchData.data.episodes.filter((item: any) => item.vote_count <= voteCountsAvg);
+            const remainingRecords = await episodeResults.filter((item: any) => item.vote_count <= voteCountsAvg);
             remainingRecords.sort((a: any, b: any) => (a.vote_average > b.vote_average) ? -1 : 1)
             filteredRecords = [...filteredRecords, ...remainingRecords];
         }
@@ -33,8 +40,7 @@ export const fetchSeriesDetailsById = async (tvId: string, seasonId: string): Pr
                 'averageVotes': value.vote_average
             };
         });
-        response.episodes = mapedData;
-        return response;
+        return mapedData;
     } catch (error) {
         const errorMessage = error && error.response && error.response.data ? error.response.data.status_message : error.message;
         const statusCode = error && error.response && error.response.status ? error.response.status : 500;
